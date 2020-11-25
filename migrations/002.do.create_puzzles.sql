fc28d986c2b235686e45e988e01fc9fcce2bf878
Ben Hernandez <benjammin234@gmail.com>
2020-11-24 17:12:31 -0800

    Decided on 2d array for clues data type

--- migrations/002.do.create_puzzles.sql
+++ migrations/002.do.create_puzzles.sql
@@ -1,6 +1,5 @@
 CREATE TABLE puzzles (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
     user_name TEXT REFERENCES users(user_name) ON DELETE CASCADE NOT NULL,
     title TEXT NOT NULL,
     rows INTEGER NOT NULL,
@@ -8,8 +7,5 @@ CREATE TABLE puzzles (
     -- figure out cells. can you do boolean || text ??
     cells BOOLEAN || TEXT [] NOT NULL,
     cellId INTEGER NOT NULL,
-    -- figure out clues. can you store an array of objects? is it helpful in any way to store clues in a separate table?
-    -- create type?
-    -- enum?
-    clues TEXT NOT NULL
+    clues TEXT [][] NOT NULL
 )
